9f16880b244f7cfcfa52afdd7b5bbf395231a0be
Shanshan-shan <2651351234@qq.com>
2019-10-03 15:43:51 +0800

    修改bug

--- Test4/Test4/exe4_2.c
+++ Test4/Test4/exe4_2.c
@@ -4,11 +4,36 @@
 //在数论中，水仙花数（Narcissistic number）也称为自恋数、自幂数、阿姆斯壮数或阿姆斯特朗数（Armstrong number），是指一N位数，其各个数之N次方和等于该数。
 //例如153、370、371及407就是三位数的水仙花数，其各个数之立方和等于该数：
 #include<stdio.h>
+#include<math.h>
+
 int main()
 {
 	int i = 0;
+	for (i = 1; i < 1000; i++)
+	{                         
+		int tmp = i;         
+		int n = 0;
+		int sum = 0;
+		//1.先计算位数n;
+		while (tmp)
+		{
+			n++;
+			tmp = tmp / 10;
+		}
+		//2.计算每一位数字的n次方和sum；
+		tmp = i;
+		while (tmp)
+		{
+			sum = sum + (int)pow(tmp % 10, n);  //pow(a,b)=a^b,返回值类型为double;
+			tmp = tmp / 10;
+		}
+		//3.比较
+		if (i == sum)
+			printf("%d ",i);
+	}
 	
-	for (i = 0; i < 1000; i++)
+	
+	/*for (i = 0; i < 1000; i++)
 	{
 		int n = i;
 		if (n / 10 == 0)
@@ -40,6 +65,6 @@ int main()
 				printf("%d ", n);
 			}
 		}
-	}
+	}*/
 	return 0;
 }
