28d1930e22cb5679e2bc6e3b3b658d7f2556e4a6
Shanshan-shan <2651351234@qq.com>
2019-10-05 15:10:13 +0800

    更新代码

--- Test5/Test5/exe5_1.c
+++ Test5/Test5/exe5_1.c
@@ -1,70 +1,68 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-//1.完成猜数字游戏。 
-#include<stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-void menu()
-{
-	printf("********************\n");
-	printf("****** 1.play ******\n");
-	printf("****** 0.exit ******\n");
-	printf("********************\n");
-}
-
-void game()
-{
-	//1.生成随机值
-	int ret = 0;
-	int guess = 0;
-	ret = rand()%100+1; //srand:产生随机数起点，用时间戳实现；rand()%100+1;（产生1-100之间的随机数）
-	              //rand在定义里可以找到随机值的最大值
-	//printf("%d\n",ret);
-	//2.猜数字
-	while (1)
-	{
-		printf("请猜数字：");
-		scanf("%d",&guess);
-		if (guess < ret)
-		{
-		printf("猜小了\n");
-		}
-		else if(guess > ret)
-		{
-			printf("猜大了\n");
-		}
-		else
-		{
-			printf("恭喜你，猜对了\n");
-			break;
-		}
-	}
-}
-
-int main()
-{
-	//这句放在主函数里面，这样调用一次,产生的随机数更加随机
-	srand((unsigned int)time(NULL));//time:时间戳，返回当前时间点与计算机开始时间的差值
-	int input = 0;
-	//printf("请选择：\n");
-	//scanf("%d",&input);
-	do
-	{
-		menu();
-		printf("请选择：\n");
-		scanf("%d", &input);
-		switch (input)
-		{
-		case 1:
-			game();
-			break;
-		case 0:
-			printf("退出游戏！\n");
-			break;
-		default:
-			printf("请做出正确选择！\n");
-			break;
-		}
-	} while (input);
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS 1
+////1.完成猜数字游戏。 
+//#include<stdio.h>
+//#include <stdlib.h>
+//#include <time.h>
+//
+//void menu()
+//{
+//	printf("********************\n");
+//	printf("****** 1.play ******\n");
+//	printf("****** 0.exit ******\n");
+//	printf("********************\n");
+//}
+//
+//void game()
+//{
+//	//1.生成随机值
+//	int ret = 0;
+//	int guess = 0;
+//	ret = rand()%100+1; //srand:产生随机数起点，用时间戳实现；rand()%100+1;（产生1-100之间的随机数）
+//	              //rand在定义里可以找到随机值的最大值
+//	//printf("%d\n",ret);
+//	//2.猜数字
+//	while (1)
+//	{
+//		printf("请猜数字：");
+//		scanf("%d",&guess);
+//		if (guess < ret)
+//		{
+//		printf("猜小了\n");
+//		}
+//		else if(guess > ret)
+//		{
+//			printf("猜大了\n");
+//		}
+//		else
+//		{
+//			printf("恭喜你，猜对了\n");
+//			break;
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	//这句放在主函数里面，这样调用一次,产生的随机数更加随机
+//	srand((unsigned int)time(NULL));//time:时间戳，返回当前时间点与计算机开始时间的差值
+//	int input = 0;
+//	do                            //do while循环至少执行一次
+//	{
+//		menu();
+//		printf("请选择：\n");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			game();
+//			break;
+//		case 0:
+//			printf("退出游戏！\n");
+//			break;
+//		default:
+//			printf("请做出正确选择！\n");
+//			break;
+//		}
+//	} while (input);
+//	return 0;
+//}
--- Test5/Test5/exe5_2.c
+++ Test5/Test5/exe5_2.c
@@ -1,2 +1,40 @@
 //#define _CRT_SECURE_NO_WARNINGS 1
-//2.写代码可以在整型有序数组中查找想要的数字， 找到了返回下标，找不到返回 - 1.（折半查找）
+////2.写代码可以在整型有序数组中查找想要的数字， 找到了返回下标，找不到返回 - 1.（折半查找）
+////二分查找；
+//#include<stdio.h>
+//
+//int main()
+//{
+//	int arr [] = {1,3,5,6,7,8,9};
+//	int k = 9;                    //寻找数组中的数字k,输出下标
+//	int sz = sizeof(arr) / sizeof(arr[0]);//求数组长度
+//	int left = 0;
+//	int right = sz - 1;
+//
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (k < arr[mid])
+//		{
+//			right = mid - 1;
+//		}
+//		else if (k > arr[mid]) //一定要用else if；用if会出问题，
+//			                   //多个if语句时即使前一个if为真，后面的if也要继续判断
+//							   //如果用else if 则只有前者为假的时候它的if 才接着判断
+//							   //else if：不是上一个条件的前提下，如果是这个条件；
+//		{
+//			left = mid + 1;
+//		}
+//	    else
+//		{
+//			printf("找到了，下标是：%d\n",mid);
+//			break;
+//		}
+//	}
+//	if (left > right)
+//	{
+//		printf("很遗憾，找不到\n");
+//	}
+//
+//	return 0;
+//}
