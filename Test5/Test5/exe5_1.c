92e1b0f4ebf99e8485f7b01480855bfe3c1ef957
Shanshan-shan <2651351234@qq.com>
2019-10-05 12:11:12 +0800

    更新代码

--- Test5/Test5/exe5_1.c
+++ Test5/Test5/exe5_1.c
@@ -1,6 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 //1.完成猜数字游戏。 
 #include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void menu()
 {
@@ -10,8 +12,39 @@ void menu()
 	printf("********************\n");
 }
 
+void game()
+{
+	//1.生成随机值
+	int ret = 0;
+	int guess = 0;
+	ret = rand()%100+1; //srand:产生随机数起点，用时间戳实现；rand()%100+1;（产生1-100之间的随机数）
+	              //rand在定义里可以找到随机值的最大值
+	//printf("%d\n",ret);
+	//2.猜数字
+	while (1)
+	{
+		printf("请猜数字：");
+		scanf("%d",&guess);
+		if (guess < ret)
+		{
+		printf("猜小了\n");
+		}
+		else if(guess > ret)
+		{
+			printf("猜大了\n");
+		}
+		else
+		{
+			printf("恭喜你，猜对了\n");
+			break;
+		}
+	}
+}
+
 int main()
 {
+	//这句放在主函数里面，这样调用一次,产生的随机数更加随机
+	srand((unsigned int)time(NULL));//time:时间戳，返回当前时间点与计算机开始时间的差值
 	int input = 0;
 	//printf("请选择：\n");
 	//scanf("%d",&input);
@@ -23,7 +56,7 @@ int main()
 		switch (input)
 		{
 		case 1:
-			printf("开始游戏！\n");
+			game();
 			break;
 		case 0:
 			printf("退出游戏！\n");
