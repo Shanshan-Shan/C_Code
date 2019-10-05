8d5bac82b230a964986ab78a9e33edc60a8a8d04
Shanshan-shan <2651351234@qq.com>
2019-10-05 16:19:48 +0800

    更新代码

--- Test5/Test5/exe5_4.c
+++ Test5/Test5/exe5_4.c
@@ -1,3 +1,27 @@
-//#define _CRT_SECURE_NO_WARNINGS 1
+#define _CRT_SECURE_NO_WARNINGS 1
 //4.编写一个程序，可以一直接收键盘字符， 如果是小写字符就输出对应的大写字符，
 //如果接收的是大写字符，就输出对应的小写字符，如果是数字不输出。
+#include<stdio.h>
+
+int main()
+{
+	int ch = 0;
+	while ((ch = getchar()) != EOF)
+	{
+		if (ch >= 'a'&&ch <= 'z')
+		{
+			putchar(ch - 32);       //ASCII码表
+		}
+		else if (ch >= 'A'&&ch <= 'Z')
+		{
+			putchar(ch + 32);
+		}
+		else if (ch >= '0'&&ch <= '9')
+		{
+			;
+		}
+		else
+			putchar(ch);
+	}
+	return 0;
+}
