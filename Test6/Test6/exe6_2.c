ddda6231d58daf40a33473fec845c306c666e4f6
Shanshan-shan <2651351234@qq.com>
2019-10-06 12:59:58 +0800

    更新代码

--- Test6/Test6/exe6_2.c
+++ Test6/Test6/exe6_2.c
@@ -1,21 +1,21 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-//2.使用函数实现两个数的交换
-#include<stdio.h>
-
-int Exchange(i, j)
-{
-	int tmp = 0;
-	tmp = i;
-	i = j;
-	j = tmp;
-	printf("i=%d j=%d\n",i,j);
-}
-
-int main()
-{
-	int i = 10;
-	int j = 20;
-	printf("i=%d j=%d\n", i, j);//printf里面可不敢&，否则i，j会变成随机数
-	Exchange(i, j);
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS 1
+////2.使用函数实现两个数的交换
+//#include<stdio.h>
+//
+//int Exchange(i, j)
+//{
+//	int tmp = 0;
+//	tmp = i;
+//	i = j;
+//	j = tmp;
+//	printf("i=%d j=%d\n",i,j);
+//}
+//
+//int main()
+//{
+//	int i = 10;
+//	int j = 20;
+//	printf("i=%d j=%d\n", i, j);//printf里面可不敢&，否则i，j会变成随机数
+//	Exchange(i, j);
+//	return 0;
+//}
